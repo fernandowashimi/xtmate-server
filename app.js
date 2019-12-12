18d7f1264c2e492b90770da22cbe903079370fd8
Fernando Shinji <shinji@biggy.com.br>
2019-12-12 18:47:32 -0300

    remove localhost

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use((req, res, next) => {
 
 io.on('connection', socket => {
     socket.on('user_left', async user => {
+        console.log('emit close' + user);
         await axios.put('http://localhost:8080/room/5deef9a41c9d44000092d53c/remove', user);
     });
 });
